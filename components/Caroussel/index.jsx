f2d349ff788e9ad5afcce6a9271c3375122a163e
Luca <lucamaia2007@hotmail.com>
2023-01-17 16:12:29 -0300

    evolucao 1

--- components/Caroussel/index.jsx
+++ components/Caroussel/index.jsx
@@ -1,52 +1,25 @@
 import Carousel from 'react-bootstrap/Carousel';
 import styles from"./Caroussel.module.css"
 
-export default function CarouselComponent() {
+export default function CarouselComponent({link}) {
     return (
         <Carousel fade >
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://st2.depositphotos.com/4164031/6914/i/450/depositphotos_69145633-stock-photo-flag-of-brazil.jpg"
-                    alt="First slide"
-                    id={styles.luca}
-                    // style={{height:"70vh"}}
-                />
-                <Carousel.Caption>
-                    <h3>Encontro de Bambas 2020</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://burst.shopifycdn.com/photos/perfect-yellow-flower.jpg?width=1200&format=pjpg&exif=1&iptc=1"
-                    alt="Second slide"
-                    id={styles.luca}
-                    // style={{height:"70vh"}}
-                />
-
-                <Carousel.Caption>
-                    <h3>Encontro de Bambas 2021</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045__480.jpg"
-                    alt="Third slide"
-                    id={styles.luca}
-                    // style={{height:"70vh"}}
-                />
-
-                <Carousel.Caption>
-                    <h3>Encontro de Bambas 2022</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
+            {link.map((item,index) => {
+                return (
+                    <Carousel.Item key={index}>
+                        <img
+                            className="d-block w-100"
+                            src={item.src}
+                            alt="First slide"
+                            id={styles.luca}
+                        />
+                        <Carousel.Caption>
+                            <h3>{item.title}</h3>
+                            <p>{item.text}</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                )
+            })}
         </Carousel>
     );
 }
--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function NavbarComponent() {
     return (
@@ -14,50 +13,36 @@ export default function NavbarComponent() {
                         width={130}
                         height={70}
                     />
-                    {/*<h5>Aruanda Capoeira</h5>*/}
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto"></Nav>
                     <Nav>
-                        <Nav.Link href="#memes">
-                            Home
-                        </Nav.Link>
-
-                        <Nav.Link eventKey={2} href="#memes">
-                            Instagram
-                        </Nav.Link>
-
-                        <Nav.Link eventKey={3} href="#memes">
-                            Conheça nossa jornada
-                        </Nav.Link>
-
-                        {/*<NavDropdown title="Dropdown" id="basic-nav-dropdown">*/}
-                        {/*    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>*/}
-                        {/*    <NavDropdown.Item href="#action/3.2">*/}
-                        {/*        Another action*/}
-                        {/*    </NavDropdown.Item>*/}
-                        {/*    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>*/}
-                        {/*    <NavDropdown.Divider />*/}
-                        {/*    <NavDropdown.Item href="#action/3.4">*/}
-                        {/*        Separated link*/}
-                        {/*    </NavDropdown.Item>*/}
-                        {/*</NavDropdown>*/}
-
-                        <Nav.Link eventKey={4} href="#memes">
+                        <Nav.Link eventKey={1} href="#eventos">
                             Eventos Aruanda Capoeira
                         </Nav.Link>
 
-                        <Nav.Link eventKey={5} href="#memes">
-                            Eventos futuros
+                        <Nav.Link eventKey={2} href="#eventos">
+                            Notícias
                         </Nav.Link>
 
-                        <Nav.Link eventKey={6} href="#memes">
-                            Entre em contato
+                        <Nav.Link eventKey={3} href="https://www.instagram.com/aruanda.capoeira/">
+                            Instagram
                         </Nav.Link>
+
+                        <Nav.Link eventKey={4} href="#jornada">
+                            Conheça nossa jornada
+                        </Nav.Link>
+
+                        <Nav.Link eventKey={5} href="#mestres">
+                            Mestres da casa
+                        </Nav.Link>
+
+                        {/*<Nav.Link eventKey={6} href="">*/}
+                        {/*    Locais de Treino*/}
+                        {/*</Nav.Link>*/}
                     </Nav>
-
 
                 </Navbar.Collapse>
             </Container>
