88f77544714581165f81414fa07c849f9a23f15d
Luca <lucamaia2007@hotmail.com>
2023-02-08 15:44:01 -0300

    parallax- horários

--- subpages/Horarios/index.jsx
+++ subpages/Horarios/index.jsx
@@ -32,9 +32,9 @@ export default function Horarios() {
        <div className={styles.bg_color}>
            <div className={styles.text}>
                <Image src="/hora.svg" height={100} width={80}/>
-               <h2 className={styles.text_horarios} id="horarios">
-                  Horários Aruanda Brasília - DF
-               </h2>
+               {/*<h2 className={styles.text_horarios} id="horarios">*/}
+               {/*   Horários Aruanda Brasília - DF*/}
+               {/*</h2>*/}
                <p className={styles.text}>
                    Local :
                    <a className={styles.text}
